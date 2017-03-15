18b6b3824fbd96b60698e2f14432c77e29de6c7d
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2017-03-15 18:11:19 +0000

    Add message if redirect to event

--- src/LandingPage.vue
+++ src/LandingPage.vue
@@ -1,23 +1,27 @@
 <template>
   <main class="landingPage">
     <div class="intro">
-      <section id="title">
-        <img src="./assets/logo.svg" alt="Logo" />
-        <div class="titleText">
-          <h1>{{$t('app.landingPage.title')}}</h1>
-          <div class="subtitle">{{$t('app.landingPage.subtitle')}}</div>
-        </div>
-      </section>
-      <section id="description">
-        <p v-html="$t('app.landingPage.description')"></p>
-        <login v-show="!authenticated" :label="$t('app.landingPage.getStarted')"></login>
-        <div v-show="authenticated">
-          <router-link to="/profile" class="button"><i class="fa fa-arrow-right"></i> {{$t('app.landingPage.getStarted')}}</router-link>
-          <div class="button" @click="logout()"><i class="fa fa-sign-out"></i>{{$t('app.logout')}}</div>
-        </div>
-      </section>
+      <div class="header">
+        <section id="title">
+          <img src="./assets/logo.svg" alt="Logo" />
+          <div class="titleText">
+            <h1>{{$t('app.landingPage.title')}}</h1>
+            <div class="subtitle">{{$t('app.landingPage.subtitle')}}</div>
+          </div>
+        </section>
+        <section id="description">
+          <p v-html="$t('app.landingPage.description')"></p>
+          <login v-show="!authenticated" :label="$t('app.landingPage.getStarted')"></login>
+          <div v-show="authenticated">
+            <router-link to="/profile" class="button"><i class="fa fa-arrow-right"></i> {{$t('app.landingPage.getStarted')}}</router-link>
+            <div class="button" @click="logout()"><i class="fa fa-sign-out"></i>{{$t('app.logout')}}</div>
+          </div>
+        </section>
+      </div>
     </div>
-    <div class="description">
+    <section class="getStartedPlease" v-if="getStartedToContinue" v-html="$t('app.landingPage.getStatedToContinue')">
+    </section>
+    <div class="description" v-else>
       <section id="time">
         <h3><i class="fa fa-calendar"></i> {{$t('app.landingPage.timeTitle')}}</h3>
         <p v-html="$t('app.landingPage.time')"></p>
@@ -50,7 +54,8 @@
     data() {
       return {
         user: 'admin',
-        authenticated: Auth.isAuthenticated()
+        authenticated: Auth.isAuthenticated(),
+        getStartedToContinue: false
       }
     },
     methods: {
@@ -58,6 +63,9 @@
         Auth.logout()
         document.location.reload()
       }
+    },
+    created() {
+      this.getStartedToContinue = this.$route.query && this.$route.query.redirect
     }
   }
 </script>
