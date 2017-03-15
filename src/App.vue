a24fec08aaf00a51cf15bf092f47e758ddb70d83
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2017-03-15 18:53:22 +0000

    Add footer with contact and license

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,31 @@
     <transition name="fade" mode="out-in" appear>
       <router-view></router-view>
     </transition>
+    <footer>
+      <ul>
+        <li><a class="action" href="mailto:admin@roganizr.io?Subject=Organiz.io">{{$t('app.footer.contact')}}</a></li>
+        <li><div class="action" @click="showEulaModal = true">{{$t('app.footer.eula')}}</div></li>
+      </ul>
+    </footer>
+    <modal v-if="showEulaModal" @close="showEulaModal = false">
+      <h3 slot="header">{{$t('app.footer.eula')}} (MIT)</h3>
+      <p slot="body">
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+      </p>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from './components/Modal.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {Modal},
+  data() {
+    return {
+      showEulaModal: false
+    }
+  }
 }
 </script>
 
---
+++ src/components/Modal.vue
@@ -0,0 +1,35 @@
+<template>
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              default body
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              <div class="modal-default-button button" @click="$emit('close')">
+                OK
+              </div>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+  export default {
+    name: 'modal'
+  }
+</script>
