4a32d06103136bc91f2e6e0607e2eb616cdeba72
Pierre-Louis Renaudin <pl.renaudin@gmail.com>
2017-03-16 19:12:53 +0000

    Improve sharer

--- src/components/Sharer.vue
+++ src/components/Sharer.vue
@@ -1,39 +1,42 @@
 <template>
   <section class="sharer">
     <h2><i class="fa fa-share-alt"></i> {{$t('app.share.title')}}</h2>
-    <div class="urlShare">
-      <label>{{$t('app.share.url')}}</label>
-      <input type="text" :value="url" onClick="this.setSelectionRange(0, this.value.length)" readonly>
-    </div>
     <div class="buttons">
       <ul class="share-buttons">
         <li>
           <a :href="`https://www.facebook.com/sharer/sharer.php?u=${this.url}&t=${this.title}!`" title="Share on Facebook" target="_blank">
-            <img alt="Share on Facebook" src="../assets/img/Facebook.svg" />
+            <img alt="Share on Facebook" src="../assets/img/Facebook.svg" width="32" heigth="32" />
           </a>
         </li>
         <li>
           <a :href="`https://twitter.com/intent/tweet?source=${this.url}&text=${this.title}!:%20${this.url}`" target="_blank" title="Tweet">
-            <img alt="Tweet" src="../assets/img/Twitter.svg" />
+            <img alt="Tweet" src="../assets/img/Twitter.svg" width="32" heigth="32" />
           </a>
         </li>
         <li>
           <a :href="`https://plus.google.com/share?url=${this.url}`" target="_blank" title="Share on Google+">
-            <img alt="Share on Google+" src="../assets/img/Google+.svg" />
+            <img alt="Share on Google+" src="../assets/img/Google+.svg" width="32" heigth="32" />
           </a>
         </li>
         <li>
           <a :href="`http://www.linkedin.com/shareArticle?mini=true&url=${this.url}&title=${this.title}!&summary=${this.title}&source=${this.url}`" target="_blank" title="Share on LinkedIn">
-            <img alt="Share on LinkedIn" src="../assets/img/LinkedIn.svg" />
+            <img alt="Share on LinkedIn" src="../assets/img/LinkedIn.svg" width="32" heigth="32" />
           </a>
         </li>
         <li>
           <a :href="`mailto:?subject=${this.title}!&body=${this.title}:%20${this.url}`" target="_blank" title="Send email">
-            <img alt="Send email" src="../assets/img/Email.svg" />
+            <img alt="Send email" src="../assets/img/Email.svg" width="32" heigth="32" />
           </a>
+        </li>
+        <li>
+          <div class="urlShare">
+            <label for="eventUrl">{{$t('app.share.url')}}</label>
+            <input type="text" id="eventUrl" :value="url" onClick="this.setSelectionRange(0, this.value.length)" readonly>
+          </div>
         </li>
       </ul>
     </div>
+
   </section>
 </template>
 <script>
