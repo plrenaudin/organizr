4adec0424a3d43eb9e53cca5ff9ee62bcc38b39d
plrenaudin <pl.renaudin@gmail.com>
2020-01-13 14:15:55 +0100

    update

--- update-all.sh
+++ update-all.sh
@@ -2,5 +2,5 @@
 
 ./update-front.sh
 docker-compose -f docker-compose.prod.yml build
-docker-compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml stop
 docker-compose -f docker-compose.prod.yml up -d
